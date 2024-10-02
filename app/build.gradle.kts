d2cf465d2a561093034466519d289e4fdbc339e2
nain <126972030+nain-F49FF806@users.noreply.github.com>
2024-10-02 09:39:33 +0000

    update github release ci file
    update upload-artifact action version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
     defaultConfig {
         minSdk = rootProject.extra["minSdkVersion"] as Int
         targetSdk = rootProject.extra["targetSdkVersion"] as Int
-        versionCode = 1727860000
+        versionCode = 1727861000
         versionName = "2024.10.02"
         applicationId = "alt.nainapps.aer"
         vectorDrawables {
