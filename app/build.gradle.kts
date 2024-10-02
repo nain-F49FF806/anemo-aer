2898d56accfe2feffc0b3a55d19ec34414eacb05
nain <126972030+nain-F49FF806@users.noreply.github.com>
2024-10-02 11:44:47 +0000

    Remove 'Android.bp': we don't support it

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
     defaultConfig {
         minSdk = rootProject.extra["minSdkVersion"] as Int
         targetSdk = rootProject.extra["targetSdkVersion"] as Int
-        versionCode = 1727864000
+        versionCode = 1727869000
         versionName = "2024.10.02"
         applicationId = "alt.nainapps.aer"
         vectorDrawables {
